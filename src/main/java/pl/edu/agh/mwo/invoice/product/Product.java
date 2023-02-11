96601f5af90533c10052c5b29cb0cdbb4963bc98
Izabela Klosinska <klosinskaiza@gmail.com>
2023-02-11 11:36:07 +0100

    Almost all tests work

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,13 +10,32 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+
+        if(name == null){
+            throw new IllegalArgumentException("wrong name");
+        }
+
+        if(name.isEmpty()){
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+
+        //if(name == null || name.equals ("")){
+        //            throw new IllegalArgumentException("wrong name");
+        //        }
+        if(price == null){
+            throw new IllegalArgumentException("price cannot be null");
+        }
+        if(price.compareTo(BigDecimal.ZERO)<0){
+            throw new IllegalArgumentException("price cannot be less than 0");
+        }
+
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
+
     }
 
     public String getName() {
-
         return this.name;
     }
 
